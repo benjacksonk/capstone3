2804ac31cb286d5bead4f4e314bd9e88dc1332a7
Benjamin Jackson <benjacksonk@gmail.com>
2023-06-28 15:07:04 -0400

    added tests using BaseDaoTest and MySqlProductDao, added .properties files to gitignore

--- src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
+++ src/test/java/org/yearup/data/mysql/BaseDaoTestClass.java
@@ -4,20 +4,25 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.yearup.configuration.TestDatabaseConfig;
+import org.yearup.configurations.DatabaseConfig;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestDatabaseConfig.class)
+@TestPropertySource(locations="classpath:application.properties") // to use test/resources/application.properties
+@ContextConfiguration(classes = DatabaseConfig.class) //was (classes = TestDatabaseConfig.class) but that's a whole different testing setup
 public abstract class BaseDaoTestClass
 {
     @Autowired
     protected DataSource dataSource;
+    //set dataSource variables for: url, username, and password in: test/resources/application.properties
 
+    //not being used anymore
+    /*
     @AfterEach
     public void rollback() throws SQLException
     {
@@ -27,4 +32,5 @@ public abstract class BaseDaoTestClass
             dataSource.getConnection().rollback();
         }
     }
+    */
 }
--- src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
+++ src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
@@ -1,25 +1,25 @@
 package org.yearup.data.mysql;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.yearup.models.Product;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MySqlProductDaoTest extends BaseDaoTestClass
 {
     private MySqlProductDao dao;
 
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         dao = new MySqlProductDao(dataSource);
     }
 
+    //not being used anymore
+    /*
     @Test
     public void getById_shouldReturn_theCorrectProduct()
     {
@@ -44,5 +44,47 @@ class MySqlProductDaoTest extends BaseDaoTestClass
         // assert
         assertEquals(expected.getPrice(), actual.getPrice(), "Because I tried to get product 1 from the database.");
     }
+    */
 
+    @Test
+    void search_matches() {
+        List<Product> products = dao.search(2, new BigDecimal(75), new BigDecimal(150), "");
+
+        assertEquals(false, products.isEmpty());
+    }
+
+    @Test
+    void search_noMatches() {
+        List<Product> products = dao.search(-2, new BigDecimal(-150), new BigDecimal(-75), "v0idyllic");
+
+        assertEquals(true, products.isEmpty());
+    }
+
+    @Test
+    void listByCategoryId_matches() {
+        List<Product> products = dao.listByCategoryId(1);
+
+        assertEquals(false, products.isEmpty());
+    }
+
+    @Test
+    void listByCategoryId_noMatches() {
+        List<Product> products = dao.listByCategoryId(-2);
+
+        assertEquals(true, products.isEmpty());
+    }
+
+    @Test
+    void getById_match() {
+        Product product = dao.getById(1);
+
+        assertInstanceOf(Product.class, product);
+    }
+
+    @Test
+    void getById_noMatch() {
+        Product product = dao.getById(-2);
+
+        assertNull(product);
+    }
 }
